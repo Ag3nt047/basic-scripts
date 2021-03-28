216242398a8171e8a383fb282c5a70d3d4a10e0d
Weird2 <davidpaulweir@outlook.com>
2021-03-28 16:22:48 +1100

    update

--- scans/pingsweep.sh
+++ scans/pingsweep.sh
@@ -1,1 +1,2 @@
-for i in {1..255} ;do (ping -c 1 192.168.1.$i | grep "bytes from" &) ;done
+#for i in {1..255} ;do (ping -c 1 192.168.1.$i | grep "bytes from" &) ;done
+for ip in $(seq 1 255); do (ping -c 1 192.168.177.$ip | grep "bytes from" | cut -d " " -f 4 | cut -d ":" -f1 &);  done
